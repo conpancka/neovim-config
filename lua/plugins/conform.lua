491b4d325d6b2432968fce99512d954ec844cc61
conpancka <thunkity@gmail.com>
2025-09-06 12:32:15 -0500

    better conform config

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,24 +1,35 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    require("conform").setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-        c = { "clang_format" },
-        cpp = { "clang_format" },
-        ["_"] = { "trim_whitespace" },
-      },
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				c = { "clang_format" },
+				cpp = { "clang_format" },
+				["_"] = { "trim_whitespace" },
+			},
 
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      },
+			formatters = {
+				clang_format = {
+					prepend_args = {
+						"-style={BasedOnStyle: LLVM, UseTab: Always, IndentWidth: 4, TabWidth: 4}",
+					},
+				},
+				stylua = {
+					prepend_args = { "--indent-type", "Tabs", "--indent-width", "4" },
+				},
+			},
 
-      notify_on_error = true,
-      notify_no_formatters = true,
-    })
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
 
-    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  end,
+			notify_on_error = true,
+			notify_no_formatters = true,
+		})
+
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
 }
