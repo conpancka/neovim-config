b09f2d9226e25c541b49d8cbd48676faa1581e44
conpancka <thunkity@gmail.com>
2025-11-11 20:06:22 -0600

    removed oil

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,9 +1,12 @@
 return {
 	"catppuccin/nvim",
-	lazy = false,
 	name = "catppuccin",
+	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme "catppuccin"
-	end
+		require("catppuccin").setup({
+			transparent_background = true,
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
